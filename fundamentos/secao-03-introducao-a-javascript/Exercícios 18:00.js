8ea379548c12d1dc1d5808fbc58fa9f50ce24458
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-23 18:53:40 -0300

    Exercício 5

--- fundamentos/secao-03-introducao-a-javascript/Exercícios 18:00.js
+++ fundamentos/secao-03-introducao-a-javascript/Exercícios 18:00.js
@@ -1,4 +1,4 @@
-// Exercício 1
+        // Exercício 1
 
 // const a = 6
 // const b = 5
@@ -12,7 +12,7 @@
 
 // console.log(resultado);
 
-// Exercício 2
+        // Exercício 2
 
 // const dez = 10
 // const quinze = 15
@@ -29,14 +29,35 @@
 
 // console.log(resultado);
 
-// Exercício 3 
+        // Exercício 3 
 
-const numb = 0
+// const numb = 0
 
-if (numb > 0){
-    console.log('Positive');
-} else if (numb < 0) {
-    console.log('Negative');
-} else {
-    console.log('Zero');
+// if (numb > 0){
+//     console.log('Positive');
+// } else if (numb < 0) {
+//     console.log('Negative');
+// } else {
+//     console.log('Zero');
+// }
+
+        // Exercício 4
+
+let peca = 'Rei'
+
+
+switch (peca.toLowerCase()) {
+    case 'rainha': console.log('Diagonal, horizontal e vertical.');
+        
+        break;
+    case 'rei': console.log('Qualquer Direção');
+        
+        break;   
+    case 'Bispo': console.log('Diagonal');
+        
+        break;
+    case 'Peão': console.log('Apenas uma casa para frente');
+       
+        break;     
 }
+
