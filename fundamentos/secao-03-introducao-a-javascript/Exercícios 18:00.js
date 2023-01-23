c88fffb4bbf54dc19c7e34c08fdd9a7d27464a42
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-23 18:26:31 -0300

    Exercício 2

--- fundamentos/secao-03-introducao-a-javascript/Exercícios 18:00.js
+++ fundamentos/secao-03-introducao-a-javascript/Exercícios 18:00.js
@@ -1,12 +1,33 @@
-const a = 6
-const b = 5
-let resultado = 0
+// Exercício 1
 
-if (a>b) {
-   resultado = 'a'
-} else {
-   resultado = 'b' 
-}
+// const a = 6
+// const b = 5
+// let resultado = 0
 
-console.log(resultado);
+// if (a>b) {
+//    resultado = 'a'
+// } else {
+//    resultado = 'b' 
+// }
+
+// console.log(resultado);
+
+// Exercício 2
+
+// const dez = 10
+// const quinze = 15
+// const vinte = 20
+// let resultado = 0
+
+// if (dez > quinze && dez > vinte) {
+//     resultado = dez
+// } else if (quinze > dez && quinze > vinte) {
+//     resultado = quinze
+// } else {
+//     resultado = vinte
+// }
+
+// console.log(resultado);
+
+// Exercício 3 
 
