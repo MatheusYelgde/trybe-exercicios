7be696cdf1e8eea98b726ec91d3a4184d04b36c6
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-23 18:12:31 -0300

    Exercícios Seção 03

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-01.js
@@ -0,0 +1,12 @@
+const myName = "Matheus";
+const birthCity = "Campina Grande" ;
+let birthYear = 1998;
+
+console.log(myName,birthCity,birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+
+
