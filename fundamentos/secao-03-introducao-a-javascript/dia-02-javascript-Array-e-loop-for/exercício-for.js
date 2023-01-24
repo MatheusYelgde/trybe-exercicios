7550834d7fbe24f7909640bc9d386ae1a3144333
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-24 18:38:06 -0300

    Exercícios 1-4

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-Array-e-loop-for/exercício-for.js
@@ -0,0 +1,24 @@
+        //Exercício 1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+console.log(numbers);
+
+        //Exercício 2
+let soma = 0
+
+for(index = 0; index < numbers.length ; index += 1)
+ soma = soma + numbers[index]
+ console.log(soma)
+        
+        //Exercício 3
+let media = 0
+for(index = 0; index < numbers.length ; index += 1)
+ media = soma/numbers.length
+ console.log(media);
+       
+        //Exercício 4
+
+if (media > 20){
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor ou igual a 20');
+}
