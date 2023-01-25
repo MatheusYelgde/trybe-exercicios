0737b0aac326c0f08fa8b9348448ec95faf1485e
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-25 18:03:41 -0300

    final

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-Array-e-loop-for/dora.js
@@ -0,0 +1,42 @@
+let impar = []
+
+for(let index = 1 ; index <= 50 ; index += 2){
+    impar.push(index)  
+    console.log(impar);
+}
+
+let inicial = 0
+let final = 51
+
+
+
+
+
+
+let fruits = [3, 4, 10, 1, 12];
+let soma = 0
+
+for(let index = 0 ; index < fruits.length ; index += 1){
+    soma += fruits[index]  
+}    
+   if(soma > 15){
+    console.log(soma);
+   } else {
+    console.log('Valor menor que 16');
+   }
+
+
+
+   const n = 9 ;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);  
+
+
+
+
+
+
--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-Array-e-loop-for/exercício-for.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-Array-e-loop-for/exercício-for.js
@@ -88,3 +88,4 @@ for (let index = 0; index < numeros.length; index += 1){
 
 
 console.log(div);
+
