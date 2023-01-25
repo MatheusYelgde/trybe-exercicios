249381caa5aa00386f8b4d1fbcd0e7f785f1257c
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-25 19:41:24 -0300

    Exercícios 1 e 2

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javacript-logica-de-programacao-e-algoritmos/secao03-dia03.js
@@ -0,0 +1,21 @@
+        // Exercício 1
+
+let fatorial = 1;
+
+
+for (let index = 10 ; index > 0 ; index-=1){
+fatorial *= index
+}
+console.log(fatorial);
+
+
+        // Exercício 2
+
+let palavra ='trybe'
+let palavraInvertida = ''
+
+for(let index = 4 ; index >= 0 ; index -= 1){
+
+    palavraInvertida += palavra[index]
+}
+console.log(palavraInvertida);
