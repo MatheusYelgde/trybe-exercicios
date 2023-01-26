6b7e1451dbf7f6bfc25341ef8020cccec9dea382
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-26 19:23:11 -0300

    Exercícios 7 e 8

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javacript-objetos-e-funcoes/exercicios-dia04.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javacript-objetos-e-funcoes/exercicios-dia04.js
@@ -53,3 +53,15 @@ let leitor = {
 
 
 console.log('O livro favorito de',leitor.nome+' '+leitor.sobrenome,'se chama '+leitor.livrosFavoritos[0].titulo);
+
+     // Exercício 7
+
+leitor.livrosFavoritos.push( {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+});
+
+     // Exercício 8
+
+ console.log(leitor.nome,' tem ',leitor.livrosFavoritos.length, 'livros favoritos');    
