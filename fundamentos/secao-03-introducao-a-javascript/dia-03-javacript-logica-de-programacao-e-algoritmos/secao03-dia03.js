cec94ae6313f7c0138a179e0fc9ea661deeaf77d
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-26 18:06:59 -0300

    Exercício ainda não finalizado

--- fundamentos/secao-03-introducao-a-javascript/dia-03-javacript-logica-de-programacao-e-algoritmos/secao03-dia03.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javacript-logica-de-programacao-e-algoritmos/secao03-dia03.js
@@ -19,3 +19,37 @@ for(let index = 4 ; index >= 0 ; index -= 1){
     palavraInvertida += palavra[index]
 }
 console.log(palavraInvertida);
+
+        // Exercício 3
+        
+let array = ['java', 'javascript', 'python', 'html', 'css'];        
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+        if (array[index].length > biggestWord.length) {
+          biggestWord = array[index];
+        }
+      }
+          
+ for (let index = 0; index < array.length; index += 1) {
+        if (array[index].length < smallestWord.length) {
+          smallestWord = array[index];
+        }
+      }
+      
+      
+        // Exercício 4
+
+let maiorPrimo = 2
+let lista = []
+
+for(let index = 2 ; index <= 50 ; index+=1){
+        lista.push(index);
+        
+   if(lista[index] > maiorPrimo){
+     maiorPrimo = lista[index]
+   };     
+};
+
