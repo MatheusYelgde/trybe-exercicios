6c587c20b1781469253417d18f1d4af7959bfc15
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-26 19:03:59 -0300

    Exercício 6

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javacript-objetos-e-funcoes/exercicios-dia04.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javacript-objetos-e-funcoes/exercicios-dia04.js
@@ -21,3 +21,35 @@ let info = {
  }     
 
       // Exercício 4
+
+  for(let key in info){
+  console.log(info[key]);
+  }      
+
+      // Exercício 5
+
+ let info2 = {
+  personagem:'Tio Patinhas',
+  origem:"Christmas on Bear Mountain , Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};    
+
+
+      // Exercício 6
+      
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+  {
+    titulo: 'O Pior Dia de Todos',
+    autor: 'Daniela Kopsch',
+    editora: 'Tordesilhas',  
+   },
+  ],
+};     
+
+
+console.log('O livro favorito de',leitor.nome+' '+leitor.sobrenome,'se chama '+leitor.livrosFavoritos[0].titulo);
