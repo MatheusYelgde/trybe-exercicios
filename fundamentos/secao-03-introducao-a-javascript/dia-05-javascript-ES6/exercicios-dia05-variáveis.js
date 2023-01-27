77d7475f4a31110d9984184d32ed7fe6c4720b47
Matheus Yelgde <yelgde.8@gmail.com>
2023-01-27 15:03:28 -0300

    Três primeiros exercícios

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-ES6/exercicios-dia05-variáveis.js
@@ -0,0 +1,29 @@
+        // Exercício 1
+
+
+// function imprimeIdade() {
+//   for (let idade = 30; idade <= 40; idade += 1) {
+//     console.log('Idade dentro do for:', idade)
+//   }
+//  }
+// imprimeIdade()
+
+        // Exercício 2
+
+//   // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+//   let pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
+//   pessoa = {
+//     nome: 'Luna',
+//     idade: 19
+//   } // Altere essa estrutura para corrigir o erro.
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);        
+
+         // Exercício 3
+
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hamburguer';
+// console.log(favoriteFood);         
